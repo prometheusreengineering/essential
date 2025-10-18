b840431838cf46f79c6e7489ca1a6f52da861482
dxxxxy <owner@dreamys.studio>
2025-10-18 13:49:20 -0400

    chore: ✨ spacing ✨ + format + cleanup + remove prefix from unique

--- src/main/java/studio/dreamys/prometheus/Prometheus.java
+++ src/main/java/studio/dreamys/prometheus/Prometheus.java
@@ -12,11 +12,13 @@ import java.lang.reflect.Method;
 
 public class Prometheus implements PreLaunchEntrypoint {
     private static final Logger logger = LogManager.getLogger("Prometheus");
+
     @Override
     public void onPreLaunch() {
         MixinBootstrap.init();
         Mixins.addConfiguration("prometheus.mixins.json");
         logger.debug("getUnvisitedCount() = {}", Mixins.getUnvisitedCount());
+
         try {
             chainLoadMixins();
         } catch (ReflectiveOperationException e) {
@@ -24,19 +26,23 @@ public class Prometheus implements PreLaunchEntrypoint {
         }
     }
 
-    // Shoutout to Essential's loader code for this method
+    //https://github.com/EssentialGG/EssentialLoader/blob/master/stage2/fabric/src/main/java/gg/essential/loader/stage2/EssentialLoader.java#L180
     public static void chainLoadMixins() throws ReflectiveOperationException {
         if (Mixins.getUnvisitedCount() != 0) {
             MixinEnvironment environment = MixinEnvironment.getDefaultEnvironment();
             Object transformer = environment.getActiveTransformer();
+
             Field processorField = transformer.getClass().getDeclaredField("processor");
             processorField.setAccessible(true);
+
             Object processor = processorField.get(transformer);
+
             Method select = processor.getClass().getDeclaredMethod("select", MixinEnvironment.class);
             select.setAccessible(true);
             select.invoke(processor, environment);
         }
     }
+
     public static Logger getLogger() {
         return logger;
     }
